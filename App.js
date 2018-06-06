6597fcf508612f474601cfc1cd96e6d7f57092f1
Arjun Gupta <arjungupta@Arjuns-MacBook-Pro.local>
2018-06-06 23:53:48 +0530

    upd

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-
-
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, AppRegistry, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
-    return (
-      <Text>Hello wocytyytsrld!</Text>
-    );
-  }
+
+class Greetings extends Component{
+
+    render(){
+        return (
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
 }
 
+export default class LotsOfGreetings extends Component{
+    render(){
+        return(
+            <View style = {{ alignItems: 'center'}}>
+                <Greetings name = 'Arjun'/>
+                <Greetings name = 'Komal'/>
+                <Greetings name = 'ABCB'/>
+            </View>
+        );
+    }
+}
+
+
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
