4efc34a73fa189baa0a2bed14a0a437f742df7c1
Arjun Gupta <arjungupta@Arjuns-MacBook-Pro.local>
2018-06-07 01:01:03 +0530

    input

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import React, {Component} from 'react';
+import { AppRegistry, View, Text, TextInput} from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
-  render() {
-    return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
-      </View>
-    );
-  }
+export default class PizzaTranslator extends Component{
+    constructor (props){
+        super(props);
+        this.state = {text: ''};
+    }
+
+    render(){
+        return (
+            <View style = {{padding : 30}}>
+                <TextInput
+                style = {{height: 40}}
+                placeholder = "Type here to translate!"
+                onChangeText = {(text) => this.setState({text})}
+                />
+            <Text style = {{padding: 30, fontSize: 42}}>
+                {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+            </Text>
+            </View>
+        );
+    }
 }
 
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);
